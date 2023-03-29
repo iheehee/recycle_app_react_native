0cee29b0af9a95328d211b848c2ffc04101673c6
iheehee <iheeheedev@gmail.com>
2023-03-29 16:49:14 +0900

    tab navigation

--- recycle_app/navigation/Main.js
+++ recycle_app/navigation/Main.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Challenge from '../screens/Main/Challenge';
+import Recycle from '../screens/Main/Recycle';
+import Store from '../screens/Main/Store';
+import Profile from '../screens/Main/Profile';
 
-export default () => (
-    <View>
-        <Text>helloooooooooo</Text>
-    </View>   
-);
+const Tab = createBottomTabNavigator();
+
+const TabNavigation = () => {
+    return (
+        <Tab.Navigator initialRouteName="Store">
+            <Tab.Screen name="Challenge" component={Challenge} />
+            <Tab.Screen name="Recycle" component={Recycle} />
+            <Tab.Screen name="Store" component={Store} />
+            <Tab.Screen name="Profile" component={Profile} />
+        </Tab.Navigator>
+    )
+};
+
+export default TabNavigation;
