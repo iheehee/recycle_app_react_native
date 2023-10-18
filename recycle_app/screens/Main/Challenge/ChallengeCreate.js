4017334587523b6f6be0420d6752a2d95b3e8b02
H <h@Hui-MacBookPro.local>
2023-10-18 12:50:10 +0900

    create challenge

--- recycle_app/screens/Main/Challenge/ChallengeCreate.js
+++ recycle_app/screens/Main/Challenge/ChallengeCreate.js
@@ -1,35 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import { ScrollView, View } from "react-native";
 import styled from "styled-components/native";
-import { Text } from "@rneui/themed";
-import { Input } from "@rneui/themed";
-
-const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
+import { Text, Button } from "@rneui/themed";
 
 const BgContainer = styled.View`
   width: 100%;
   flex: 1;
   background-color: white;
 `;
-
-const StyledText = styled.Text`
-  font-size: 30px;
+const FrequencyContainer = styled.View``;
+const DurationContainer = styled.View``;
+const Input = styled.TextInput`
+  width: 350px;
+  padding: 12.5px 20px;
+  border: 1px solid grey;
+  background-color: white;
+  border-radius: 30px;
+  margin-bottom: 10px;
+  font-weight: 500;
 `;
 
 export default () => {
+  let data = [0, 1, 2, 3, 4];
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const activeStyle = { backgroundColor: "gray" };
+  const buttonStyle = {
+    title: { color: "black" },
+    button: { borderColor: "#BBBBBB" },
+  };
+
   return (
     <BgContainer>
       <Text h3>챌린지를 만들어주세요!</Text>
       <Text h4>챌린지 제목</Text>
-      <Input
-        placeholder="INPUT WITH ERROR MESSAGE"
-        inputContainerStyle={{ borderWidth: 1, paddingHorizontal: 10 }}
-        errorStyle={{ color: "red" }}
-        errorMessage="ENTER A VALID ERROR HERE"
-      />
+      <Input />
+      <FrequencyContainer>
+        <Text h4>인증 빈도</Text>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          {data.map((item, idx) => (
+            <Button
+              title="버튼입니다."
+              type="outline"
+              titleStyle={buttonStyle.title}
+              containerStyle={{
+                width: 100,
+                marginHorizontal: 5,
+                marginVertical: 5,
+                borderRadius: 10,
+                borderWidth: 0.05,
+              }}
+              buttonStyle={
+                idx === selectedIndex ? activeStyle : buttonStyle.button
+              }
+              onPress={() => setSelectedIndex(idx)}
+              activeOpacity={0.9}
+            />
+          ))}
+        </ScrollView>
+      </FrequencyContainer>
+      <DurationContainer>
+        <Text h4>챌린지 기간</Text>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          {data.map((item, idx) => (
+            <Button
+              title="버튼입니다."
+              type="outline"
+              titleStyle={buttonStyle.title}
+              containerStyle={{
+                width: 100,
+                marginHorizontal: 5,
+                marginVertical: 5,
+                borderRadius: 10,
+                borderWidth: 0.05,
+              }}
+              buttonStyle={
+                idx === selectedIndex ? activeStyle : buttonStyle.button
+              }
+              onPress={() => setSelectedIndex(idx)}
+              activeOpacity={0.9}
+            />
+          ))}
+        </ScrollView>
+      </DurationContainer>
     </BgContainer>
   );
 };
--- recycle_app/util/Ip.js
+++ recycle_app/util/Ip.js
@@ -1,3 +1,3 @@
-//const localIp = "http://192.168.35.47:8080";
+//const localIp = "http://192.168.35.126:8080";
 const localIp = "http://192.168.0.55:8080";
 export default { localIp };
